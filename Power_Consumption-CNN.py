7b237745ba5309d242dac7a7a19f125e5565fa15
Adarsh Pal <39993662+aadypal@users.noreply.github.com>
2019-05-09 01:28:42 +0530

    Update Power_Consumption-CNN.py

--- Power_Consumption-CNN.py
+++ Power_Consumption-CNN.py
@@ -1,4 +1,3 @@
-#load and clean-up data\
 import numpy as np
 import pandas as pd
 import keras
@@ -12,7 +11,7 @@ from keras.layers import Flatten
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
  
-# fill missing values with a value at the same time one day ago
+#fill missing values with a value at the same time one day ago
 def fill_missing_data(values):
 	one_day = 60 * 24
 	for row in range(values.shape[0]):
@@ -20,38 +19,33 @@ def fill_missing_data(values):
 			if np.isnan(values[row, col]):
 				values[row, col] = values[row - one_day, col]
  
-# load given data
+#load raw data
 dataset = pd.read_csv('household_power_consumption.txt', sep=';', header=0, low_memory=False, infer_datetime_format=True, parse_dates=             
                      {'datetime':[0,1]}, index_col=['datetime'])
-# mark all missing values
+#mark all missing values
 dataset.replace('?', np.nan, inplace=True)
-# make dataset numeric
 dataset = dataset.astype('float32')
-# fill missing
+#fill missing
 fill_missing_data(dataset.values)
-# add a column for for the remainder of sub metering
+#add a column for for the remainder of sub metering
 values = dataset.values
 dataset['sub_metering_4'] = (values[:,0] * 1000 / 60) - (values[:,4] + values[:,5] + values[:,6])
-# save updated dataset
+#save updated dataset
 dataset.to_csv('household_power_consumption.csv')
 
-# load the new file
+#load saved file
 dataset = pd.read_csv('household_power_consumption.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
-# resample data to daily
+#grouping to daily data
 daily_groups_data = dataset.resample('D')
 daily_data = daily_groups_data.sum()
-# summarize
 print(daily_data.shape)
-# save
 daily_data.to_csv('household_power_consumption_days.csv')
 
 #plot dataset
 dataset = pd.read_csv('household_power_consumption_days.csv', header=0, index_col=0)
 values = dataset.values
-# specify columns to plot
 groups = [0, 1, 2, 3, 5, 6, 7]
 i = 1
-# plot each column
 plt.figure()
 for group in groups:
 	plt.subplot(len(groups), 1, i)
@@ -61,27 +55,20 @@ for group in groups:
 plt.show()
 
 # multichannel multi-step cnn
-# split a multivariate dataset into train/test sets
 def split_dataset(data):
-	# split into standard weeks
 	train, test = data[1:-328], data[-328:-6]
-	# restructure into windows of weekly data
 	train = np.array(np.split(train, len(train)/7))
 	test = np.array(np.split(test, len(test)/7))
 	return train, test
  
-# evaluate one or more weekly forecasts against expected values
 def evaluate_forecasts(actual, predicted):
 	scores = list()
-	# calculate an RMSE score for each day
+	#RMSE score for each day
 	for i in range(actual.shape[1]):
-		# calculate mse
 		mse = mean_squared_error(actual[:, i], predicted[:, i])
-		# calculate rmse
 		rmse = sqrt(mse)
-		# store
 		scores.append(rmse)
-	# calculate overall RMSE
+	#overall RMSE
 	s = 0
 	for row in range(actual.shape[0]):
 		for col in range(actual.shape[1]):
@@ -89,38 +76,28 @@ def evaluate_forecasts(actual, predicted):
 	score = sqrt(s / (actual.shape[0] * actual.shape[1]))
 	return score, scores
  
-# summarize scores
 def summarize_scores(name, score, scores):
 	s_scores = ', '.join(['%.1f' % s for s in scores])
 	print('%s: [%.3f] %s' % (name, score, s_scores))
  
-# convert history into inputs and outputs
 def to_supervised(train, n_input, n_out=7):
-	# flatten data
 	data = train.reshape((train.shape[0]*train.shape[1], train.shape[2]))
 	X, y = list(), list()
 	in_start = 0
-	# step over the entire history one time step at a time
 	for _ in range(len(data)):
-		# define the end of the input sequence
 		in_end = in_start + n_input
 		out_end = in_end + n_out
-		# ensure we have enough data for this instance
 		if out_end < len(data):
 			X.append(data[in_start:in_end, :])
 			y.append(data[in_end:out_end, 0])
-		# move along one time step
+		#move along one time step
 		in_start += 1
 	return np.array(X), np.array(y)
  
-# train the model
 def build_model(train, n_input):
-	# prepare data
 	train_x, train_y = to_supervised(train, n_input)
-	# define parameters
-	verbose, epochs, batch_size = 0, 70, 16
+	verbose, epochs, batch_size = 1, 90, 16
 	n_timesteps, n_features, n_outputs = train_x.shape[1], train_x.shape[2], train_y.shape[1]
-	# define model
 	model = Sequential()
 	model.add(Conv1D(filters=32, kernel_size=3, activation='relu', input_shape=(n_timesteps,n_features)))
 	model.add(Conv1D(filters=32, kernel_size=3, activation='relu'))
@@ -131,55 +108,41 @@ def build_model(train, n_input):
 	model.add(Dense(100, activation='relu'))
 	model.add(Dense(n_outputs))
 	model.compile(loss='mse', optimizer='adam')
-	# fit network
 	model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=verbose)
 	return model
  
-# make a forecast
+#make a forecast
 def forecast(model, history, n_input):
-	# flatten data
 	data = np.array(history)
 	data = data.reshape((data.shape[0]*data.shape[1], data.shape[2]))
-	# retrieve last observations for input data
 	input_x = data[-n_input:, :]
-	# reshape into [1, n_input, n]
 	input_x = input_x.reshape((1, input_x.shape[0], input_x.shape[1]))
 	# forecast the next week
 	yhat = model.predict(input_x, verbose=0)
-	# we only want the vector forecast
 	yhat = yhat[0]
 	return yhat
  
-# evaluate a single model
 def evaluate_model(train, test, n_input):
-	# fit model
 	model = build_model(train, n_input)
-	# history is a list of weekly data
 	history = [x for x in train]
-	# walk-forward validation over each week
+	#walk-forward validation over each week
 	predictions = list()
 	for i in range(len(test)+1):
-		# predict the week
+		#predict the week
 		yhat_sequence = forecast(model, history, n_input)
 		predictions.append(yhat_sequence)
 		while i<len(test):
 			history.append(test[i, :])
-            
-	# evaluate predictions days for each week
 	forecast_sequence = array(predictions)
 	score, scores = evaluate_forecasts(test[:, :, 0], predictions)
 	return score, scores, forecast_sequence
  
-# load the new file
 dataset = pd.read_csv('household_power_consumption_days.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
-# split into train and test
 train, test = split_dataset(dataset.values)
-# evaluate model and get scores
 n_input = 14
 score, scores, forecast_sequence = evaluate_model(train, test, n_input)
-# summarize scores
 summarize_scores('cnn', score, scores)
-# plot scores
+#plot scores
 days = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat']
 plt.xlabel('Day')
 plt.ylabel('Loss in KiloWatt for whole day')
